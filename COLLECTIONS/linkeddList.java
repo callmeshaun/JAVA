54baa87c6e95f2a1ad1f34c81082532ea0c8d1ee
shaunakkk <161214557+callmeshaun@users.noreply.github.com>
2025-01-15 13:53:45 +0000

    LINKED LIST

--- COLLECTIONS/linkeddList.java
+++ COLLECTIONS/linkeddList.java
@@ -12,8 +12,10 @@ public static void main(String args[])
     a1.add(0, 59);
     a1.addAll(1,a2);
     a1.set(0, 69);
+    a1.addFirst(32);
+    a1.addLast(100);
     
-    
+    System.out.println("the first element is"+a1.peek() );
     a1.forEach(n->show(n));
     
 }
